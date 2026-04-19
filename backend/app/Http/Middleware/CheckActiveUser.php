dbfd270d397ab7ed484fe94edd2a1e905aa13330
fall0898 <cheikhahmettidianefall4@gmail.com>
2026-04-19 00:06:31 +0000

    fix: CheckActiveUser return type Response au lieu de JsonResponse (bloquait Excel/PDF)

--- backend/app/Http/Middleware/CheckActiveUser.php
+++ backend/app/Http/Middleware/CheckActiveUser.php
@@ -4,11 +4,11 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckActiveUser
 {
-    public function handle(Request $request, Closure $next): JsonResponse
+    public function handle(Request $request, Closure $next): Response
     {
         $user = $request->user();
 
